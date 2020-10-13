f29f9113bbcf0e2bee5adeb11045c2336cf53866
manci <javiercaliescali@gmail.com>
2020-10-13 16:54:05 -0500

    work on home  view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => (
   <div className="container">
     <Row>
-      <Col lg={12}>
-        <h3>WHAT WE DO</h3>
-        <p>
+      <Col lg={{ span: 8, offset: 2 }}>
+        <h3 className="text-center">WHAT WE DO</h3>
+        <p className="text-center mt-3">
           Our organization seeks to research, develop,
           and implement engineering practices, designs, products,
           and methods that promote social and environmental justice
@@ -20,45 +23,133 @@ const Home = () => (
         </p>
       </Col>
     </Row>
-    <Row>
-      Subscribe
+    <Row className="mt-5">
+      <Col>
+        <h3 className="text-center">SUBSCRIBE</h3>
+        <p className="text-center">Sign up to receive news and updates from us!.</p>
+      </Col>
     </Row>
-    <Row>
+    <Row className="mt-3">
+      <Col>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} md={{ span: 2.5, offset: 2 }} controlId="validationCustom01">
+              <Form.Control
+                required
+                type="text"
+                placeholder="First Name"
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="2.6" controlId="validationCustom01">
+              <Form.Control
+                required
+                type="text"
+                placeholder="Last Name"
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="2.6" controlId="validationCustom01">
+              <Form.Control
+                required
+                type="email"
+                placeholder="Email"
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="3">
+              <Button type="submit" variant="info" className="signup">SIGN UP</Button>
+            </Form.Group>
+          </Form.Row>
+        </Form>
+      </Col>
+    </Row>
+    <Row className="mt-5">
       <Col>
         <h3>Environmental Justice</h3>
-        <p>Environmental justice is the fair treatment and meaningful involvement of all people regardless of race, color, national origin, or income, with respect to the development, implementation, and enforcement of environmental laws, regulations, and policies, (EPA).</p>
-        <p>We will take this a step further and include the consideration of all inhabitants, including animals.</p>
+        <ul>
+          <li>
+            <p>
+              Environmental justice is the fair treatment and meaningful
+              involvement of all people regardless of race, color, national origin,
+              or income, with respect to the development, implementation,
+              and enforcement of environmental laws, regulations, and policies, (EPA).
+            </p>
+          </li>
+          <li>
+            <p>
+              We will take this a step further and include the consideration
+              of all inhabitants, including animals.
+            </p>
+          </li>
+        </ul>
       </Col>
       <Col>
         <h3>Ethical Engineering</h3>
-        <p>
-          Engineers, in the fulfillment of their professional duties,
-          shall: Hold paramount the safety, health, and welfare of the public.
-        </p>
-        <p>
-          Perform services only in areas of their competence.
-          Issue public statements only in an objective and truthful manner.
-        </p>
-        <p>
-          Act for each employer or client as faithful agents or trustees.
-          Avoid deceptive acts. Conduct themselves honorably, responsibly, 
-          ethically, and lawfully so as to enhance the honor, reputation, 
-          and usefulness of the profession. (I. Fundamental Cannons, National
-          Society of Professional Engineers).
-        </p>
-        <p>Each of these cannons completely compliments social and environmental justice. </p>
-        <p>
-          Our initial work will be to compose these three similar definitions
-          and subjects into a guiding principles that will allow our
-          organization to fulfill our goals and bylaws.
-        </p>
+        <ul>
+          <li>
+            <p>
+              Engineers, in the fulfillment of their professional duties,
+              shall: Hold paramount the safety, health, and welfare of the public.
+            </p>
+          </li>
+          <li>
+            <p>
+              Perform services only in areas of their competence.
+              Issue public statements only in an objective and truthful manner.
+            </p>
+          </li>
+          <li>
+            <p>
+              Act for each employer or client as faithful agents or trustees.
+              Avoid deceptive acts. Conduct themselves honorably, responsibly,
+              ethically, and lawfully so as to enhance the honor, reputation,
+              and usefulness of the profession. (I. Fundamental Cannons, National
+              Society of Professional Engineers).
+            </p>
+          </li>
+          <li>
+            <p>Each of these cannons completely compliments social and environmental justice. </p>
+          </li>
+          <li>
+            <p>
+              Our initial work will be to compose these three similar definitions
+              and subjects into a guiding principles that will allow our
+              organization to fulfill our goals and bylaws.
+            </p>
+          </li>
+        </ul>
       </Col>
       <Col>
         <h3>Social Justice</h3>
-        <p>The fair and proper administration of laws conforming to the natural law that all persons, irrespective of ethnic origin, gender, possessions, race, religion, etc., are to be treated equally and without prejudice, (Business Directory). </p>
-        <p>Further, social justice is the virtue which guides us in creating those organized human interactions we call institutions. </p>
-        <p>In turn, social institutions, when justly organized, provide us with access to what is good for the person, both individually and in our associations with others. </p>
-        <p>Social justice also imposes on each of us a personal responsibility to work with others, at whatever level of the “Common Good” in which we participate, to design and continually perfect our institutions as tools for personal and social development, (Center for Economic and Social Justice).</p>
+        <ul>
+          <li>
+            <p>
+              The fair and proper administration of laws conforming
+              to the natural law that all persons, irrespective of ethnic origin,
+              gender, possessions, race, religion, etc., are to be treated equally
+              and without prejudice, (Business Directory).
+            </p>
+          </li>
+          <li>
+            <p>
+              Further, social justice is the virtue which guides us
+              in creating those organized human interactions we call institutions.
+            </p>
+          </li>
+          <li>
+            <p>
+              In turn, social institutions, when justly organized, provide
+              us with access to what is good for the person, both individually
+              and in our associations with others.
+            </p>
+          </li>
+          <li>
+            <p>
+              Social justice also imposes on each of us a personal responsibility
+              to work with others, at whatever level of the “Common Good” in which
+              we participate, to design and continually perfect our institutions as
+              tools for personal and social development, (Center for Economic and Social Justice).
+            </p>
+          </li>
+        </ul>
       </Col>
     </Row>
   </div>
