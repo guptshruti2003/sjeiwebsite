34d04c4027b7a5ed2dcf5f2e431d71c2ca005842
manci <javiercaliescali@gmail.com>
2020-10-16 17:14:06 -0500

    work on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import { SocialIcon } from 'react-social-icons';
+import logoBottom from '../img/guidestart.svg';
 
 const Footer = () => (
   <footer className="bg-dark text-white">
@@ -29,10 +30,22 @@ const Footer = () => (
       </div>
       <div className="bottom">
         <div className="mt-3">
-          <p className="text-center">THE SOCIAL JUSTICE + ENGINEERING INITIATIVE, 2 NORTH CENTRAL AVENUE, SUITE 1800, PHOENIX, AZ, 85004,</p>
-          <p className="text-center">ITE 1800, PHOENIX, AZ, 85004,</p>
+          <p className="text-center white">THE SOCIAL JUSTICE + ENGINEERING INITIATIVE, 2 NORTH CENTRAL AVENUE, SUITE 1800, PHOENIX, AZ, 85004,</p>
+          <p className="text-center white">ITE 1800, PHOENIX, AZ, 85004,</p>
 
-          <p className="text-center">The Social Justice + Engineering Initiative is a 501 (c)(3) non-profit organization.</p>
+          <p className="text-center gray">The Social Justice + Engineering Initiative is a 501 (c)(3) non-profit organization.</p>
+          <div className="guidelogo text-center">
+            <a href="https://www.guidestar.org/profile/82-1792979" target="blank">
+              <img
+                src={logoBottom}
+                width="140"
+                height="140"
+                className="d-inline-block align-top"
+                alt="guide-start"
+              />
+            </a>
+          </div>
+          <p className="text-center gray">Powered by AstroCivEng</p>
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,12 +18,12 @@ const NavBar = () => (
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Nav className="ml-auto">
-      <NavLink to="/" className="mr-5 link">SJEI</NavLink>
-      <NavLink to="/what-we-do" className="mr-5 link">ABOUT</NavLink>
-      <NavLink to="/take-action" className="mr-5 link">TAKE ACTION</NavLink>
-      <NavLink to="/press-release" className="mr-5 link">PRESS RELEASE</NavLink>
-      <a href="https://medium.com/sjei" target="blank" className="anchor mr-5">BLOG</a>
-      <NavLink to="/donation" className="mr-5 link">SUPPORT</NavLink>
+      <NavLink to="/" className="mr-4 link">SJEI</NavLink>
+      <NavLink to="/what-we-do" className="mr-3 link">ABOUT</NavLink>
+      <NavLink to="/take-action" className="mr-3 link">TAKE ACTION</NavLink>
+      <NavLink to="/press-release" className="mr-3 link">PRESS RELEASE</NavLink>
+      <a href="https://medium.com/sjei" target="blank" className="anchor mr-3">BLOG</a>
+      <NavLink to="/donation" className="mr-2 link support">SUPPORT</NavLink>
     </Nav>
   </Navbar>
 );
