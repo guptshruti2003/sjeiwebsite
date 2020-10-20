9040eabd2b6f1cbcb894a69705df06a300083cad
manci <javiercaliescali@gmail.com>
2020-10-20 14:40:49 -0500

    work on william g bentz and board page

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,46 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import About from './About';
 
 const Board = () => (
-  <div></div>
+  <div className="container">
+    <Row>
+      <About />
+      <Col md={7} className="container">
+        <Row className="block">
+          <p>
+            <a href="https://www.linkedin.com/in/ryanwbentz/" target="blank" className="anchor-t">Ryan William Bentz - CEO, Executive Director & Founder </a>
+            | Project Engineer, 5x5 Technologies
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/josephayala/" target="blank" className="anchor-t">Joseph Ayala - Assistant Executive Director, Programming Director & Founder </a>
+            | T-Mobile
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/unavailable/" target="blank" className="anchor-t">Kerrine Bryan - Engineering & Development Director and Founder  </a>
+            | MEng MBA CEng FIET &  Electrical Engineer, WSP USA
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/kortnee-carmack-mpa-06bb373b/" target="blank" className="anchor-t">Kortnee Carmack - Fundraising & Grant Director </a>
+            | MPA - Manager, Grants & Planning, Cook Inlet Tribal Council, Inc.
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/ryanwbentz/" target="blank" className="anchor-t">Colleen Casey, P.E. - Engineering & Development Chair </a>
+            | Founder & CEO, Caseco Engrg Solutions, LLC
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/dagnysignorelli/" target="blank" className="anchor-t">Dagny Signorelli - Environmental Justice Subcommittee Chair </a>
+            | M.Sc. Forest Ecology and Sustainable Management, Swedish University of
+            Agricultural Sciences (2022)
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/cindy-roberts-17ba3176/" target="blank" className="anchor-t">Cindy Roberts - Social Justice Subcommittee Chair </a>
+            | HQ PACAF, Force Support Inspections Chief, TS SCI at The United States Air Force
+          </p>
+        </Row>
+      </Col>
+    </Row>
+  </div>
 );
 export default Board;
--- src/components/William.js
+++ src/components/William.js
@@ -1,6 +1,37 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import About from './About';
 
 const William = () => (
-  <div></div>
+  <div className="container">
+    <Row>
+      <About />
+      <Col md={7} className="container">
+        <Row className="block">
+          <h5 className="special-note-title">A special note</h5>
+          <p>
+            I started The Social Justice + Engineering Initiative in June of 2017
+            to honor my father William &quot;Bill&quot; Gordon Bentz. He later lost his incredible
+            battle to incurable cancer and other complications on September 9th, 2017.
+          </p>
+          <p>He will never be forgotten. His memory will live on through the work of SJEI. </p>
+          <p>
+            My father blindly accepted people for who they are and helped those who needed it
+            - unconditionally.
+          </p>
+          <p>
+            His greatest wish was to know that he guided me. I hope that he rests in comfort
+            to know that he was the one person in my life who had the greatest impact on me.
+          </p>
+          <p>
+            Because of this, I want to be able to do good for the public benefit and in his
+            honor.
+          </p>
+          <p>I love you dad. </p>
+        </Row>
+      </Col>
+    </Row>
+  </div>
 );
 export default William;
