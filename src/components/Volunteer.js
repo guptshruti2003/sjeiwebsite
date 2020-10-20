437caedf59fa42a5c995dd930c0f8f596bba2b4f
manci <javiercaliescali@gmail.com>
2020-10-20 18:03:34 -0500

    work on volunteer

--- src/components/Volunteer.js
+++ src/components/Volunteer.js
@@ -10,14 +10,278 @@ const Corporate = () => (
       <Col md={7} className="container">
         <Row className="block">
           <Col>
-            <h4>Administrative Assistant Volunteer</h4>
-            <p>
-              Virtual Opportunity
-              Grow with our non-profit organization by
-              providing much needed support to our all volunteer
-              organization. Help us lay the foundation of this new
-              organization and work to provide ...
-            </p>
+            <div>
+              <h4>Administrative Assistant Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by
+                providing much needed support to our all volunteer
+                organization. Help us lay the foundation of this new
+                organization and work to provide ...
+              </p>
+            </div>
+            <div>
+              <h4>Administrative Assistant- Executive</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support
+                to our organization. Help us lay the foundation of this new organization
+                and work to provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Assistant Director Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Assistant Research Director Volunteer Primary:
+                The Research Assistant Director volunteer, at the direction of and
+                in collaboration with the Research Director and relevant Board Members will
+                oversee ...
+              </p>
+            </div>
+            <div>
+              <h4>Civil Engineer Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Civil Engineer Volunteer We are looking for industry experts, recent
+                college graduates, or those that will be completing their degree within
+                12 to 18 months, who want ...
+              </p>
+            </div>
+            <div>
+              <h4>Digital Marketing Coordinator</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to
+                our organization. Help us lay the foundation of this new organization and work
+                to provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Director of Projects</h4>
+              <p>
+                Virtual Opportunity
+                We are looking for industry experts who want to give back to their community
+                in an executive capacity by volunteering to be among the first board members
+                of The ...
+              </p>
+            </div>
+            <div>
+              <h4>Electrical Engineer Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Electrical Engineer Volunteer We are looking for industry experts who want
+                to give back to their community in a professional capacity by volunteering to
+                be among the first ...
+              </p>
+            </div>
+            <div>
+              <h4>Full Stack Engineer</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to our
+                organization. Help us lay the foundation of this new organization and work to
+                provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Fundraising & Donor Relations Coordinator</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to
+                our organization. Help us lay the foundation of this new organization and work
+                to provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Mechanical Engineer Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Mechanical Engineer Volunteer We are looking for industry experts, recent
+                college graduates, or those that will be completing their degree within 12 to
+                18 months, who want to ...
+              </p>
+            </div>
+            <div>
+              <h4>Multimedia Designer</h4>
+              <p>
+                Virtual Opportunity
+                Are you a multimedia designer with a passion for designing and producing video,
+                graphical and interactive content that inspires audiences to engage and take
+                action? Are you a storyteller ...
+              </p>
+            </div>
+            <div>
+              <h4>Program Director Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Program Director Volunteer Primary: The Program Manager volunteer at
+                the direction of and in collaboration with relevant Board Members will oversee
+                all organizational day to day functions, provide ...
+              </p>
+            </div>
+            <div>
+              <h4>Program Manager Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Program Manager Volunteer Primary: The Program Manager volunteer at the
+                direction of and in collaboration with the Projects Director and relevant
+                Board Members will oversee all organizational day ...
+              </p>
+            </div>
+            <div>
+              <h4>Project Manager Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Project Manager Volunteer Primary: The Project Manager volunteer at
+                the direction of and in collaboration with the Projects Director and relevant
+                Board Members will oversee all organizational ...
+              </p>
+            </div>
+            <div>
+              <h4>Projects Director Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Projects Director Volunteer Primary: The Project Director volunteer at the
+                direction of and in collaboration with the Board of Directors, and Board Members
+                will oversee all organizational day ...
+              </p>
+            </div>
+            <div>
+              <h4>Research Director Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                Assistant Research Director Volunteer Primary: The Research Director volunteer,
+                at the direction of and in collaboration with the Board of Directors and
+                relevant Board Members will oversee ...
+              </p>
+            </div>
+            <div>
+              <h4>Social Justice Program Manager</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to our
+                organization. Help us lay the foundation of this new organization and work to
+                provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Social Media + Marketing Manager</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to our
+                organization. Help us lay the foundation of this new organization and work to
+                provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Civil Engineering Subcommittee Chair</h4>
+              <p>
+                Virtual Opportunity
+                Civil Engineering Subcommittee Chair We are looking for industry experts
+                who want to give back to their community in an executive capacity by
+                volunteering to be ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Communications & Marketing Coordinator</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to
+                our all volunteer organization. Help us lay the foundation of this new
+                organization and work to provide ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Fundraising & Grants Committee Chair</h4>
+              <p>
+                Virtual Opportunity
+                We are looking for industry experts who want to give back to their community
+                in an executive capacity by volunteering to be among the first board members
+                of The ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer General Counsel</h4>
+              <p>
+                Virtual Opportunity
+                We are looking for industry experts who want to give back to their community
+                in an executive capacity by volunteering to be among the first board members
+                of The ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Graphic Designer</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to
+                our all volunteer organization. Help us lay the foundation of this new
+                organization and work to provide ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Mechanical/Structures Engineering Subcommittee Chair</h4>
+              <p>
+                Virtual Opportunity
+                Volunteer Mechanical/Structures Engineering Subcommittee Chair We are looking
+                for industry experts who want to give back to their community in an executive
+                capacity by volunteering to ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Operations Manager</h4>
+              <p>
+                Virtual Opportunity
+                Grow with our non-profit organization by providing much needed support to our
+                organization. Help us lay the foundation of this new organization and work to
+                provide positive technical, ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Project Coordinator</h4>
+              <p>
+                Virtual Opportunity
+                Volunteer Project Coordinator Description Primary: The Volunteer Project
+                Coordinator at the direction of and in collaboration with the Board of
+                Directors and Organization Coordinator will oversee project functions, ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Project Grant Writer and Researcher</h4>
+              <p>
+                Virtual Opportunity
+                Volunteer Project Grant Writer and Researcher: We are looking for volunteer
+                grant writing and grant research experts who want to give back to their
+                community in a project centered ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Projects Committee Chair</h4>
+              <p>
+                Virtual Opportunity
+                We are looking for industry experts who want to give back to their community
+                in an executive capacity by volunteering to be among the first board members
+                of The ...
+              </p>
+            </div>
+            <div>
+              <h4>Volunteer Social Media & Marketing Director</h4>
+              <p>
+                Virtual Opportunity
+                We are looking for industry experts who want to give back to their community
+                in an executive capacity by volunteering to be among the first board members
+                of The ...
+              </p>
+            </div>
+            <div>
+              <h4>Website Developer Volunteer</h4>
+              <p>
+                Virtual Opportunity
+                We are looking for industry experts who want to give back to their
+                community by laying the groundwork for something special. Grow with our
+                non-profit organization by ...
+              </p>
+            </div>
           </Col>
         </Row>
       </Col>
